7f25cd86f40a9968af633c5e5c9284a49edd1a5c
Roman <ratul152375@gmail.com>
2020-03-11 19:04:49 -0400

    ado

---
+++ ADONET/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ADONET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblfirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblfirstName
+            // 
+            this.lblfirstName.AutoSize = true;
+            this.lblfirstName.Location = new System.Drawing.Point(13, 34);
+            this.lblfirstName.Name = "lblfirstName";
+            this.lblfirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblfirstName.TabIndex = 0;
+            this.lblfirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(12, 57);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(58, 13);
+            this.lblLastName.TabIndex = 1;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(16, 105);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(52, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(76, 31);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(76, 54);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(16, 145);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(287, 225);
+            this.lstStudents.TabIndex = 6;
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(309, 145);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.Size = new System.Drawing.Size(354, 225);
+            this.dgvStudents.TabIndex = 8;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(16, 377);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(76, 76);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(13, 79);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 8;
+            this.lblID.Text = "ID";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(76, 105);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(52, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(245, 34);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 11;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(134, 105);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(52, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(211, 382);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(92, 13);
+            this.lblInfo.TabIndex = 12;
+            this.lblInfo.Text = "00 Records found";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 408);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblfirstName);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database Conn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblfirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ ADONET/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADONET
+{
+    public partial class Form1 : Form
+    {
+        string connStr = "Server=INSTRUCTORIT; Database=IBTCollege; User Id=ProfileUser; Password=ProfileUser2019";
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string testConn = "SELECT * FROM Students";
+                SqlCommand cmd = new SqlCommand(testConn, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                int total = 0;
+
+                while (reader.Read())
+                {
+                    string fullName = reader["firstName"] + " " + reader["lastName"];
+                    lstStudents.Items.Add(fullName);
+                    total++;
+                }
+
+                lblInfo.Text = String.Format("{0} Records found", total);
+            }            
+        }
+
+        private void RefreshData()
+        {
+            var select = "SELECT * FROM Students";
+            var c = new SqlConnection(connStr); // Your Connection String here
+            var dataAdapter = new SqlDataAdapter(select, c);
+
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dgvStudents.ReadOnly = true;
+            dgvStudents.DataSource = ds.Tables[0];
+        }
+
+        private void AddRecord()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string addRecord = String.Format("INSERT INTO Students (firstName, lastName, ID) " +
+                    "VALUES ('{0}', '{1}', {2})", txtFirstName.Text, txtLastName.Text, txtID.Text);
+                SqlCommand cmd = new SqlCommand(addRecord, conn);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                lblResult.Text = String.Format("{0}, {1}, Added into the database.", txtFirstName.Text, txtLastName.Text);
+            }
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtID.Clear();                
+        }
+
+        private void EditRecord()
+        {
+
+        }
+
+        private void DeleteRecord()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string deleteRecord = String.Format("DELETE FROM Students WHERE firstName='{0}' OR lastName='{1}' OR ID={2}", txtFirstName.Text, txtLastName.Text, txtID.Text);
+                SqlCommand cmd = new SqlCommand(deleteRecord, conn);
+                int rowsAffected = cmd.ExecuteNonQuery();                
+                lblResult.Text = String.Format("{0} Records deleted.", rowsAffected);
+            }
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtID.Clear();
+        }
+
+        private void SearchRecord()
+        {
+            lstStudents.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string SearchConn = String.Format("SELECT * FROM Students " +
+                    "WHERE firstName='{0}' OR lastName='{1}' OR ID={2}", txtFirstName.Text, txtLastName.Text, txtID.Text);
+                SqlCommand cmd = new SqlCommand(SearchConn, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                int rowsAffected = 0;                
+
+                while (reader.Read())
+                {
+                    string fullName = reader["firstName"] + " " + reader["lastName"];
+                    lstStudents.Items.Add(fullName);
+                    rowsAffected++;
+                }
+                lblResult.Text = String.Format("{0} Records found.", rowsAffected);
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            AddRecord();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            SearchRecord();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteRecord();
+        }
+    }
+}
